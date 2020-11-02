71a45533cf61beec7057101fb48585c7fd3f43bc
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-02 23:50:13 +0000

    ifConditions

---
+++ IfElse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+  int age = 20;
+  if (age >= 18)
+    {
+      printf("Ok you are adult\n");
+      
+    }
+  else 
+    printf ("Ok u r a minor \n");
+
+
+}
+
