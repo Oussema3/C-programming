2709b63ee5ae15e16a4465f34d899225c5f60c47
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-02 23:28:18 +0000

    specifiers

---
+++ variabls.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+  int age;
+  float weight;
+  char sex;
+
+  age = 23;
+  weight = 75;
+  sex = 'M';
+  printf("my age is %d , my weight os %f and my sex is %c \n",age,weight,sex);
+  return 0;
+}
