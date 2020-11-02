96e8a801e78d45f2c1b68bb1a59b5ccaff2660a9
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-02 23:42:43 +0000

    dealing with user

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+  int age;
+  float weight;
+  printf("Enter your age and weight \n");
+  scanf("%d%f",&age,&weight);
+  printf("Your age is %d and Your weight is %f \n",age,weight);
+  return 0;
+}
