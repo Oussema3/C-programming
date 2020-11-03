1d48d0023869073d17d5fab8770df834f7c5accb
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-03 00:15:40 +0000

    Logical operators

---
+++ logicO.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+  int age = 17;
+  if (age >= 15 && age <= 18)
+    {
+      printf("Okey you are a teenager \n");
+    }
+  return 0;
+  /*using OR operator 
+int age = 18;
+if (age == 18 || age == 16)
+printf("you are lucky\n");
+else 
+printf("You are not lucky\n");
+  */
+}
