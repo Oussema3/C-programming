bf683f21137c22ea74a9d21753daa6f699e951b1
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-03 00:27:04 +0000

    IncAndDec

---
+++ IncrementanDecremnt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+  int age = 10;
+  printf("age is %d \n",age);
+  printf("now age is %d \n",age++);
+  printf("finally it increments to %d \n",age);
+  return 0;
+}
