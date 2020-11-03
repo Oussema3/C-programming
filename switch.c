dae24305e7eaa4b89187e5faff9f4903fb833b6c
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-03 16:37:40 +0000

    swithing cases

---
+++ switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+  int counter = 16 ;
+  switch (counter)
+    {
+    case 1: 
+      printf("one\n");
+      break;
+    case 2:
+      printf("two\n");
+      break;
+    case 3:
+      printf("Three\n");
+	break;
+    case 4:
+      printf("four\n");
+      break;
+    default:
+      printf("None of the above\n");
+    }
+}
