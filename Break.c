61aca8c7b1acb299bf90cc981b2141b7c058b922
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-03 11:35:53 +0000

    BreakItToMeBaby

---
+++ Break.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+  int counter;
+  for (counter = 1; counter <=10 ;counter++)
+    {
+      if (counter == 5)
+	{	
+break;
+	}
+      printf("%d\n", counter);
+    }
+  printf("After the loop\n");
+}
