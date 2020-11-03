6c0422eb00df39f2150230b81f08211f1b57e0dd
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-03 11:24:52 +0000

    Dowhile

---
+++ doWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+int counter;
+
+  do {
+    printf("hello \n");
+    counter++;
+  }
+  while (counter <= 10);
+
+}
+// Note that when the condition is wrong at the very beggining the code will be exucuted ONCE and then never again 
