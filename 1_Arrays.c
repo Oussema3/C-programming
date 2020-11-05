462652f2b81157d79815dc23cfadd0196ce1fb2c
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-05 15:18:47 +0000

    Arrays

---
+++ 1_Arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main ()
+{
+
+  int mark[6] = {5, 7, 5, 89, 11, 15};
+  float avg [2];
+  avg[0]= 65.6;
+  avg[1]= 75.8;
+  printf("%d\n",mark[0]);
+  printf("%d\n",mark[1]);
+  printf("%d\n",mark[2]);
+  printf("%d\n",mark[5]);
+  printf("%f\n", avg[1]);
+  return 0;
+}
+  
