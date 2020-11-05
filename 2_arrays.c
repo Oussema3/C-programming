785a2767b328667ee98a0207964e6ec1529c0d55
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-05 15:34:22 +0000

    Arrays and User

---
+++ 2_arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+
+
+  int size = 6;
+  int marks [size];
+  int counter;
+
+  for (counter = 0 ; counter < size ; counter ++)
+    {
+      scanf("%d",&marks[counter]);
+    }
+  for (counter = 0;counter <size ; counter++)
+    {
+      printf("the elementa at %d is %d\n",counter,marks[counter]);
+    }
+	return 0;
+}
