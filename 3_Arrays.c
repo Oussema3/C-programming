7ddcf6455fce2644f5da468b2aabd5ad517e1080
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-05 15:55:32 +0000

    Arrays and strings

---
+++ 3_Arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+
+{
+  char name[] = {'M','u','s','e'};
+  char sirName[] = "Ajmi";
+
+  printf("%c\n", name[0]);
+
+  // to print the whole string
+  printf("%s\n", name);
+
+  printf("%c\n", sirName[0]);
+  return 0;
+}
+
+
